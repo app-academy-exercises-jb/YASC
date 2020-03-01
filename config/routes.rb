6ee55c49fa171c85fa5cf8206cdf3187a519ecdb
Jorge Barreto <jrbd93@gmail.com>
2020-03-01 12:51:52 -0500

    boilerplate react component bootstrapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :sessions, only: [:create, :destroy]
+    resources :users
+  end
+
+  get '*path', to: 'application#index', contraints: lambda { |req| 
+    !req.xhr? && req.format.html?
+  }
 end
