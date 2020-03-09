729ad9c4acef5f025090080c77f3af41cde0b047
Jorge Barreto <jrbd93@gmail.com>
2020-03-09 17:04:09 -0400

    fix workspace errors

--- app/javascript/components/components/session/error_wrapper.js
+++ app/javascript/components/components/session/error_wrapper.js
@@ -33,12 +33,22 @@ const ErrorWrapper = ({ err, type }) => {
   }
 }
 
-const ErrorComponent = ({ errors }) => (
-  <>{errors && <ul className="errors">
-    {Object.keys(errors).map((err,idx) => (
-      <ErrorWrapper type={err} err={{ [err]: errors[err] }} key={idx} />
-    ))}
-  </ul>}</>
-)
+const ErrorComponent = ({ errors, errorClass, errorType, errorId }) => {
+  if (!errors || Object.keys(errors).length === 0) return null;
+
+  if (errors[errorType] && errors[errorType][errorId]) {
+    errors = { [errorClass]: errors[errorType][errorId] };
+  } else if (errors[errorType]) {
+    return null;
+  }
+
+  return (
+    <ul className="errors">
+      {Object.keys(errors).map((err,idx) => (
+        <ErrorWrapper type={err} err={{ [err]: errors[err] }} key={idx} />
+      ))}
+    </ul>
+  );
+}
 
 export default ErrorComponent;
