397d11362a41f8e59ba876d3643576bbead03bb4
Jorge Barreto <jrbd93@gmail.com>
2020-03-13 00:36:39 -0400

    change webpacker environment again

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,6 @@ const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
 environment.loaders.prepend('erb', erb)
-const nodeModulesLoader = environment.loaders.get('nodeModules')
-
-if (!Array.isArray(nodeModulesLoader.exclude)) {
-  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
-  ? []
-  : [nodeModulesLoader.exclude]
-}
-nodeModulesLoader.exclude.push(/actioncable/)
+environment.loaders.delete('nodeModules')
 
 module.exports = environment
