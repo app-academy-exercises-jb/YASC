b1f3e04dd99e7597d4b87a2faac59c57e2e2aeaa
Jorge Barreto <jrbd93@gmail.com>
2020-03-12 23:58:01 -0400

    fix actioncable in production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,13 @@ const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
 environment.loaders.prepend('erb', erb)
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
+  ? []
+  : [nodeModulesLoader.exclude]
+}
+nodeModulesLoader.exclude.push(/actioncable/)
+
 module.exports = environment
