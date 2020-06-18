985860352322d95c4807c20c4c585ea28e5fe595
Jorge Barreto <jrbd93@gmail.com>
2020-06-18 09:09:04 -0400

    Squashed commit of the following:
    commit 2c55df35736df075b8fed84b84c46418dc003927
    Author: Jorge Barreto <jrbd93@gmail.com>
    Date:   Thu Jun 18 09:07:14 2020 -0400
    
        remove property from messages which causes N+1 query
    
    commit 7debe57546a886e1ffe156135fb618ce0fc302e6
    Author: Jorge Barreto <jrbd93@gmail.com>
    Date:   Thu Jun 18 09:05:53 2020 -0400
    
        fix issue where messages were being fetched twice

--- app/javascript/components/components/chat/channel_content/channel_messages.js
+++ app/javascript/components/components/chat/channel_content/channel_messages.js
@@ -1,58 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import DefaultUserImg from 'images/default_user';
 
-class ChannelMessages extends React.Component {
-  constructor(props) {
-    super(props);
+const ChannelMessages = ({
+  createNewMessage,
+  getMessages,
+  currentChannel,
+  currentWorkspace,
+  messages,
+  users,
+  user
+}) => {
+  const inputRef = useRef(),
+    chatBoxRef = useRef();
 
-    this.state = {
-      messages: [],
-      currentChannel: null,
-      loading: false
-    }
+  const [channelMessages, setChannelMessages] = useState([]);
 
-    this.inputRef = React.createRef();
-    this.chatBoxRef = React.createRef();
-    this.inputHandler = this.inputHandler.bind(this);
-  }
+  useEffect(() => {
+    let chatBox = chatBoxRef.current;
+    if (!chatBox) return;
+    chatBox.scroll({
+      top: chatBox.scrollHeight,
+      behavior: "auto"
+    });
+  }, [chatBoxRef.current]);
 
-  componentDidMount() {
-    // gimme a document event listener on keydown so we can capture input
-  }
-
-  componentWillUnmount() {
-    // remove above event listener
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    let ref = this.chatBoxRef.current;
-    if (!ref) return;
-    ref.scroll({
-      top: ref.scrollHeight,
-      behavior: this.state.loading ? "auto" : "smooth"});
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { currentChannel, messages, getMessages } = nextProps;
-    if (!currentChannel || !messages) return null;
-    if (!messages[currentChannel.id]) {
-      getMessages(currentChannel);
-      return null;
-    } else {
-      const state = { 
-        messages: messages[currentChannel.id],
-        currentChannel: nextProps.currentChannel.id
-      }
-      if (prevState.currentChannel !== nextProps.currentChannel.id) {
-        state.loading = true;
+  useEffect(() => {
+    if (currentChannel && messages) {
+      if (!messages[currentChannel.id]) {
+        getMessages(currentChannel);
       } else {
-        state.loading = false;
+        setChannelMessages(messages[currentChannel.id]);
       }
-      return state;
     }
-  }
+  }, [messages, currentChannel]);
 
-  inputHandler(e) {
+  const inputHandler = (e) => {
     if (e.target.value === "") return;
     if (e.keyCode === 13 && e.shiftKey === false) {
       e.preventDefault();
@@ -61,57 +43,63 @@ class ChannelMessages extends React.Component {
 
       e.target.value = "";
       
-      this.props.createNewMessage({ 
+      createNewMessage({ 
         body,
         channel_id
       });
     }
-  }
+  };
 
-  render() {
-    const { messages } = this.state,
-      { currentChannel, users } = this.props;
-    
-    return (
-      <div id="channel-messages-wrapper">
+  return (
+    <div id="channel-messages-wrapper">
 
-        <div ref={this.chatBoxRef} id="channel-messages-content">
-        {messages && messages.map(m => (
-            <div key={m.id} className="channel-message-wrapper">
-              <div className="channel-message-item">
-                <div className="message-icon">
-                  <img src={DefaultUserImg} />
-                </div>
-                <div className="message-content-wrapper">
-                  <span className="message-header">
-                    <p className="user-id">{users[m.author_id] && users[m.author_id].email}</p>
-                    <p className="message-time">
-                      {new Date(Date.parse(m.created_at)).toLocaleTimeString()}
-                    </p>
-                  </span>
-                  <span className="message-content">
-                    {m.body}
-                  </span>
-                </div>
+      <div ref={chatBoxRef} id="channel-messages-content">
+      {channelMessages.map(m => (
+          <div key={m.id} className="channel-message-wrapper">
+            <div className="channel-message-item">
+              <div className="message-icon">
+                <img src={DefaultUserImg} />
+              </div>
+              <div className="message-content-wrapper">
+                <span className="message-header">
+                  <p className="user-id">{users[m.author_id] && users[m.author_id].email}</p>
+                  <p className="message-time">
+                    {new Date(Date.parse(m.created_at)).toLocaleTimeString()}
+                  </p>
+                </span>
+                <span className="message-content">
+                  {m.body}
+                </span>
               </div>
             </div>
-        ))}
-        </div>
-
-
-        <div id="text-editor-wrapper">
-          <div id="text-editor-border">
-            <textarea 
-              id="text-editor"
-              ref={this.inputRef}
-              onKeyDown={this.inputHandler}
-              name={currentChannel && currentChannel.id}
-            />
           </div>
+      ))}
+      </div>
+
+
+      <div id="text-editor-wrapper">
+        <div id="text-editor-border">
+          <textarea 
+            id="text-editor"
+            ref={inputRef}
+            onKeyDown={inputHandler}
+            name={currentChannel && currentChannel.id}
+          />
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  )
+};
 
+// class ChannelMessages extends React.Component {
+
+//   componentDidMount() {
+//     // gimme a document event listener on keydown so we can capture input
+//   }
+
+//   componentWillUnmount() {
+//     // remove above event listener
+//   }
+
+  
 export default ChannelMessages;
--- app/views/api/messages/_message.json.jbuilder
+++ app/views/api/messages/_message.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! message, :id, :body, :author_id, :channel_id, :parent_message_id, :reply_ids, :created_at, :updated_at
+json.extract! message, :id, :body, :author_id, :channel_id, :parent_message_id, :created_at, :updated_at
